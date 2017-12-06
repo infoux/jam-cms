6733619b3aaa74cec0bb2ce773140fbcc0439903
Unknown <emrdl7@gmail.com>
2017-12-06 10:13:19 +0900

    트리메뉴 수정

--- js/common.js
+++ js/common.js
@@ -1,9 +1,24 @@
 $(document).ready(function() {
 
+
+  $("nav li a").each(function(){
+    if ($(this).parent().find("ul").length > 0 ) {
+      console.log("sdf");
+    } else {
+      $(this).parent().toggleClass("nodepth");
+    }
+  
+  });
+
+
+
+
   $("nav li a").click(function() {
 
-    $(this).closest("li").toggleClass("active");
-    $(this).toggleClass("active");
+    $(this).siblings().toggleClass("active");
+    $(this).parent().toggleClass("active");
+
+
 
   });
 
